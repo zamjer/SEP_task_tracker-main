2a95c787793839ff350f3d82589f5a128560c947
zamjer <zamjerwin@gmail.com>
2024-08-01 15:18:33 +1000

    Assessment 2 DevOps

---
+++ app_events/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class App_EventsConfig(AppConfig):
+    name = "app_events"
---
+++ app_events/models/event_member.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from app_accounts.models import User
+from app_events.models import Event, EventAbstract
+
+
+class EventMember(EventAbstract):
+    """ Event member model """
+
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="events")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="event_members"
+    )
+
+    class Meta:
+        unique_together = ["event", "user"]
+
+    def __str__(self):
+        return str(self.user)
